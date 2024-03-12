6883b9f07c740404d12cdd165f32dc57c83f4ba7
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-03-12 19:59:53 +0700

    All test for to total function

--- tests/total.test.js
+++ tests/total.test.js
@@ -1,9 +1,18 @@
-const total = require('../total')
+const total = require("../total");
 /* test('total of empty string should be 0', () => {
     expect(total('')).toBe(0);
 }) */
-describe('Total Test', () => {
-    it('should test that true === true', () => {
-        expect(true).toBe(true)
-    })
-})
+describe("Total Test", () => {
+  it("should test that true === true", () => {
+    expect(true).toBe(true);
+  });
+  it("should test that empty string === 0", () => {
+    expect(total("")).toBe(0);
+  });
+  it("should test that 1,2 === 3", () => {
+    expect(total("1,2")).toBe(3);
+  });
+  it("should test that 1,1001 === 1", () => {
+    expect(total("1,1001")).toBe(1);
+  });
+});
