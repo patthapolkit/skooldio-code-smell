d29630759af7e64cce69ee4d1da9ee2689bf27b7
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-03-12 23:21:42 +0700

    Add total route to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const productsRoute = require('./routes/products');
+const totalRoute = require('./routes/total');
 
 const app = express();
 
@@ -9,4 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/', productsRoute);
+app.use('/', totalRoute);
+
 module.exports = app;
