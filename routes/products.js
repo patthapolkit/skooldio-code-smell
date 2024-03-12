36bae7dbc6cb7569c004deb0d1990c0640ed2eb6
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-03-12 19:43:56 +0700

    Enable all api

--- routes/products.js
+++ routes/products.js
@@ -8,49 +8,35 @@ const convert = require('../convert')
 const products = [{ id: '1', name: 'Playstation 5', inStock: false }];
 
 router.get('/', (req, res) => {
-    console.log("Get all products");
-    console.log("All products " + products.length);
-    console.log("Before return");
     res.json(products);
 });
 
 router.get('/:id', (req, res, next) => {
-    console.log("Target id = " + req.params.id);
     const product = products.find(
         (product) => product.id === String(req.params.id)
     );
-    console.log("Search result is " + product)
     if (!product) {
-        console.log("Found nothing");
         return next(createError(404, 'Not Found'));
     }
-    console.log("Before return");
     res.json(product);
 });
 
 router.post('/', (req, res, next) => {
-    console.log("Create new product");
     const { body } = req;
     if (typeof body.name !== 'string') {
         return next(createError(400, 'Validation Error'));
     }
-    console.log("Create new ID");
     var newId = products.length + 1;
     const newProduct = { id: "'" + newId + "'", name: body.name, inStock: false, };
     products.push(newProduct);
-    console.log("Before return");
     res.status(201).json(newProduct);
 });
 
 router.post('/total', (req, res) => {
-    console.log("POST Total");
     const { body } = req;
     var values = body.values;
-    console.log("values " + values);
     var result = total(values);
-    console.log("total " + result);
     var convertResult = convert(result);
-    console.log("Before return");
     res.json(convertResult);
 });
 
